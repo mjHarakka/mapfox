0dbbcd7653d3c8672249d256d7082bc9374af84a
Aleksanteri Fagerholm <aleksanteri.fagerholm@gmail.com>
2020-02-13 14:55:33 +0200

    added mapview dependency, the user can now see the map and one artificial marker

--- MapFox/App.js
+++ MapFox/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MapView style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 60.200692,
+        longitude: 24.934302,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221,
+      }}>
+      <Marker
+        coordinate={{
+          latitude: 60.201373,
+          longitude: 24.934041
+        }} title='Haaga-Helia' />
+    </MapView>
   );
 }
 
