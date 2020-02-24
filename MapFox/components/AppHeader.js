4ef1e7fc84432bf70bef1440d9d06b6bf42ca43a
Noora-Irene <nooraetel@gmail.com>
2020-02-24 14:36:04 +0200

    buttonarea fixed

--- MapFox/components/AppHeader.js
+++ MapFox/components/AppHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function AppHeader() {
 
@@ -8,17 +8,11 @@ return (
         <View>
             <Text>Welcome to Mapfox</Text>
         </View>
-        <View style={styles.buttonArea}>
+        <View>
             <Button title="Create a new adventure?" />
             <Button title="Seek a new adventure?" />
         </View>
-
     </View>
  
     );
 }
-const styles= StyleSheet.create({
-    buttonArea: {
-        flexDirection: 'row'
-    }
-})
