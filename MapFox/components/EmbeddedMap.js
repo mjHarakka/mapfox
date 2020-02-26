002e5ad233a96216eaee91769409436205b095d3
arvilsa <sannaarvilommi@hotmail.com>
2020-02-26 15:21:48 +0200

    Base for You Are Here (small map)-component in startscreen

---
+++ MapFox/components/EmbeddedMap.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
+
+export default function EmbeddedMap() {
+
+return (
+   
+        <View>
+            <Text>You are here-block:</Text>
+        </View>
+    
+ 
+    );
+}
+
--- MapFox/screens/StartScreen.js
+++ MapFox/screens/StartScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppHeader from '../components/AppHeader';
+import EmbeddedMap from '../components/EmbeddedMap';
+
 
 const StartScreen =()=> {
     return (
+      <View>
         <AppHeader/>
+        <EmbeddedMap/>
+      </View>
+        
     )
 
 }
