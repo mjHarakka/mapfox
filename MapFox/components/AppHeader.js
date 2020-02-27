b037ff45fb5984a8e3d73744b5dd3332d91ee03f
Olli Eloranta <olli@ollieloranta.fi>
2020-02-27 16:07:41 +0200

    Added Button elements from React Native Elements into AppHeader

--- MapFox/components/AppHeader.js
+++ MapFox/components/AppHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StatusBar, View, Text, Button } from 'react-native';
+import { StatusBar, View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default function AppHeader() {
 
@@ -7,9 +8,10 @@ return (
     <View>
         <StatusBar hidden = {true} />
         <View>
-            <Text>Welcome to Mapfox</Text>
+            <Text>W elcome to Mapfox</Text>
         </View>
         <View>
+            <Button title="Test Button" />
             <Button title="Create a new adventure?" />
             <Button title="Seek a new adventure?" />
         </View>
