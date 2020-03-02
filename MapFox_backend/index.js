ef93f6809266a31b6dbf0625fcd01bd219798145
Mikko Harakka <mj.harakka@gmail.com>
2020-03-02 20:51:14 +0200

    refactored index.js, axios working

--- MapFox_backend/index.js
+++ MapFox_backend/index.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const app = express();
+
+const axios = require('axios');
+
+const getData = () => {
+	axios.get('http://open-api.myhelsinki.fi/v1/places/').then(response => {
+		console.log(response.data)
+	})
+}
+
+console.log(getData)
+
+getData()
+
+app.get('/', (req, res) => {
+    res.send()
+})
+
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
