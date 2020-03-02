95c2d8e593e7e2718ac8b7f5780b7b77de08d2ab
Olli Eloranta <olli@ollieloranta.fi>
2020-03-02 15:07:09 +0200

    AooHeader.js modified

--- MapFox/components/AppHeader.js
+++ MapFox/components/AppHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StatusBar, View, Text } from 'react-native';
-import { Button } from 'react-native-elements';
+import { StatusBar, View, StyleSheet } from 'react-native';
+import { Button, Text } from 'react-native-elements';
 
 export default function AppHeader() {
 
@@ -8,14 +8,19 @@ return (
     <View>
         <StatusBar hidden = {true} />
         <View>
-            <Text>W elcome to Mapfox</Text>
+            <Text h1>Welcome to Mapfox</Text>
         </View>
         <View>
-            <Button title="Test Button" />
             <Button title="Create a new adventure?" />
             <Button title="Seek a new adventure?" />
         </View>
     </View>
  
     );
-}
+};
+
+const styles = StyleSheet.create({
+    title: {
+
+    }
+});
