75c716ea6b20ff32f7721636f85c8224e9def387
aleksanterif <aleksanteri.fagerholm@gmail.comgit config --global user.email aleksanteri.fagerholm@gmail.com>
2020-03-24 23:00:07 +0200

    added functioning crud operations to backend, user can now add, view, update and delete event, also removed fetch from api to ease servers traffic

--- mapfox_backend/index.js
+++ mapfox_backend/index.js
@@ -1,36 +1,35 @@
 const express = require('express');
-const fetch = require('node-fetch');
-
-const cors = require('kcors');
-const Koa = require('koa');
-const router = require('koa-router')();
-
+const mongoose = require('mongoose');
 const port = process.env.PORT || 8080;
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 
 
 const mapURI = process.env.MAP_ENDPOINT || 'http://open-api.myhelsinki.fi/v1/places/';
 
-
-
-const app = new Koa();
-
+const app = express();
 app.use(cors());
+app.use(bodyParser.json());
+dotenv.config();
 
 
-const fetchLocations = async () => {
-	const endpoint = mapURI;
-	const response = await fetch(endpoint);
-	return response ? response.json() : {};
-};
 
-router.get('/api/apidata', async ctx => {
-	const locationData = await fetchLocations();
-	ctx.type = 'application/json; charset=utf-8';
-	ctx.body = locationData.data ? locationData : {};
-});
+//import routes
+const eventsRoute = require('./routes/events')
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use('/events', eventsRoute);
+
+
+//connect To DB
+mongoose.connect(process.env.DB_CONNECTION,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	},
+	() => console.log('connection succeeded')
+);
 
 app.listen(port);
 
