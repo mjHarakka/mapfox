d353464335c5cc741ef4c49c72ac7401656ab88d
MapFox Production <mj.harakka@gmail.com>
2020-03-30 21:02:36 +0000

    commit from server, had to modify index.js

--- mapfox_backend/index.js
+++ mapfox_backend/index.js
@@ -6,6 +6,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 
+const MONGO_USERNAME = 'mapfox';
+const MONGO_PASSWORD = 'BlueJeansAreCool!';
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'events';
 
 const mapURI = process.env.MAP_ENDPOINT || 'http://open-api.myhelsinki.fi/v1/places/';
 
@@ -21,14 +26,14 @@ const eventsRoute = require('./routes/events')
 
 app.use('/events', eventsRoute);
 
-
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 //connect To DB
-mongoose.connect(process.env.DB_CONNECTION,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	},
-	() => console.log('connection succeeded')
+mongoose.connect(url,
+        {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+        },
+        () => console.log('connection succeeded')
 );
 
 app.listen(port);
