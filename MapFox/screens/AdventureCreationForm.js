96921053ac3147304a9fb4a3ea98f89c1f30f30a
Mikko Harakka <mj.harakka@gmail.com>
2020-04-10 00:13:28 +0300

    basic input functionality with post working

--- MapFox/screens/AdventureCreationForm.js
+++ MapFox/screens/AdventureCreationForm.js
@@ -2,7 +2,81 @@ import React from 'react';
 import {StyleSheet, Button, View, Text, TextInput } from 'react-native';
 import axios from 'axios'
 
+const SearchForm = (props) => {
+    return (
+      <View>
+        <Text>Find countries: </Text><TextInput value={props.searchValue} onChange={props.handleSearch} />
+      </View>
+    ) 
+  }
+  
+  const CountryView = (props) => {
+    const country = props.country 
+    return (
+      <View>
+      <Text>{country.name}</Text>
+  
+  
+      </View>
+    )
+  }
+  
+  
+  
+  const Countries = (props) => {
+  
+    
+        return (
+        
+          <View>
+            {props.results.map(result => <Text key={result.alpha3Code} >{result.name}</Text>)}
+          </View>
+        )
+      
+      
+    
+}
+  
+
+const Places = () => {
+    const [countries, setCountries] = React.useState([])
+    const [searchValue, setSearchValue] = React.useState('')
+  
+    const handleSearch = (event) => {
+      console.log(event.target.value)
+      setSearchValue(event.target.value)
+    }
+  
+    const results = countries.filter(country => country.name.indexOf(searchValue) != -1)
+  
+    React.useEffect(() => {
+      console.log('effect')
+      axios
+        .get('https://restcountries.eu/rest/v2/all')
+        .then(response => {
+          console.log('promise fulfilled')
+          setCountries(response.data)
+        })
+    }, [])
+    console.log('render', countries.length, 'persons')
+  
+    return (
+
+      <View>
+      <SearchForm searchValue={searchValue} handleSearch={handleSearch} />
+      <Countries results={results}/>
+      
+  
+      </View>
+    )
+ }
+
 const AdventureCreationForm = () => {
+
+    const [title, setTitle] = React.useState('')
+    const [description, setDescription] = React.useState('')
+    const [places, setPlaces] = React.useState('')
+
 
     const [eventObject, setEventObject] = React.useState({
         title: '',
@@ -36,8 +110,8 @@ const AdventureCreationForm = () => {
         event.preventDefault
     
         const eventObject = {
-            title: 'a',
-            description: 'a',
+            title: title,
+            description: description,
             places: [{
                 name: 'b',
                 latitude: 0.0,
@@ -52,37 +126,25 @@ const AdventureCreationForm = () => {
         })
     }
 
-    /*
-    const postEventObject = () => fetch('http://128.199.36.67/events', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            title: 'a',
-            description: 'a',
-            places: [{
-                name: 'a',
-                latitude: 0.0,
-                longitude: 0.0
-            }]
-        }) 
-        .catch(err => console.error(err))
-        
-    })
-    */
    
+
     return (
         <View style={styles.container}>
             <Text>Create an adventure here</Text>
-            {/*
             <TextInput
+                placeholder={'Otsikko'}
                 style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                onChangeText={text => onChangeText(text)}
-                value={value}
+                onChangeText={title => setTitle(title)}
+                value={title}
                 />
-            */}
+            <TextInput
+                placeholder={'Kuvaus tapahtumasta'}
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={description => setDescription(description)}
+                value={description}
+            />
+            <Places />
+
             <Button title={'Press'} onPress={postEventObject}></Button>
 
         </View>
