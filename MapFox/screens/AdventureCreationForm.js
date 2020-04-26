5f7dfd9a76e114e05afec6935e9136c9b92b9b0e
aleksanterif <aleksanteri.fagerholm@gmail.comgit config --global user.email aleksanteri.fagerholm@gmail.com>
2020-04-26 22:27:51 +0300

    user can now post events to backend from mobile phone, and select places from helsinki open api

--- MapFox/screens/AdventureCreationForm.js
+++ MapFox/screens/AdventureCreationForm.js
@@ -10,10 +10,12 @@ import {
 } from "react-native";
 import axios from "axios";
 
-const PlacesView = () => {
+const AdventureCreationForm = () => {
     const [locations, setLocations] = useState([]);
     const [searchValue, setSearchValue] = useState("");
     const [searchResults, setSearchResults] = useState([]);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
     const [places, setPlaces] = useState([]);
 
     useEffect(() => {
@@ -51,82 +53,13 @@ const PlacesView = () => {
         );
     };
 
-    const logger = () => {
-        console.log(places);
-    };
-
-    return (
-        <View style={styles.places}>
-            <View style={styles.inputStyle}>
-                <TextInput
-                    placeholder={"hae paikkoja"}
-                    value={searchValue}
-                    onChangeText={(searchValue) => setSearchValue(searchValue)}
-                />
-            </View>
-            <ScrollView>
-                <View>
-                    {searchResults.map((item) => (
-                        <Text onPress={() => addPlaces(item)} key={item.id}>
-                            {" "}
-                            {item.name.fi}
-                        </Text>
-                    ))}
-                </View>
-                <Button title={"Press"} onPress={logger}></Button>
-            </ScrollView>
-        </View>
-    );
-};
-
-const AdventureCreationForm = () => {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-
-    const [eventObject, setEventObject] = useState({
-        title: "",
-        description: "",
-        places: [
-            {
-                name: "",
-                latitude: 0.0,
-                longitude: 0.0,
-            },
-        ],
-    });
-
-    const handleChange = (event) => {
-        setEventObject({ ...eventObject, [event.target.name]: event.target.value });
-    };
-
-    const addEventObject = (event) => {
-        event.preventDefault();
-        const eventObject = {
-            title: newTitle,
-            description: newDescription,
-            places: [
-                {
-                    newName: "",
-                    newLatitude: 0.0,
-                    newLongitude: 0.0,
-                },
-            ],
-        };
-    };
-
     const postEventObject = (event) => {
         event.preventDefault;
 
         const eventObject = {
             title: title,
             description: description,
-            places: [
-                {
-                    name: "b",
-                    latitude: 0.0,
-                    longitude: 0.0,
-                },
-            ],
+            places: places
         };
 
         axios
@@ -134,11 +67,11 @@ const AdventureCreationForm = () => {
             .then((response) => { });
     };
 
+    const logger = () => {
+        console.log(places);
+    };
+
     return (
-        // TODO Scroll Bar
-        // TODO Joka placesil individual lisäysnappi jolla sen saa tökättyä eventtii
-        // Postaus toimii mut places menee vaa tyhjänä listana backendii koska siihen ei oo logiikkaa atm
-        // Katotaa varmaa näist suurin osa sit yhes
         <View style={styles.container}>
             <Text>Create an adventure here</Text>
             <TextInput
@@ -153,12 +86,33 @@ const AdventureCreationForm = () => {
                 onChangeText={(description) => setDescription(description)}
                 value={description}
             />
-            <PlacesView />
 
+
+            <View style={styles.places}>
+                <View style={styles.inputStyle}>
+                    <TextInput
+                        placeholder={"hae paikkoja"}
+                        value={searchValue}
+                        onChangeText={(searchValue) => setSearchValue(searchValue)}
+                    />
+                </View>
+                <ScrollView>
+                    <View>
+                        {searchResults.map((item) => (
+                            <Text onPress={() => addPlaces(item)} key={item.id}>
+                                {" "}
+                                {item.name.fi}
+                            </Text>
+                        ))}
+                    </View>
+                </ScrollView>
+            </View>
             <Button title={"Press"} onPress={postEventObject}></Button>
         </View>
     );
 };
+
+
 
 AdventureCreationForm.navigationOptions = {
     headerTitle: "My adventures",
