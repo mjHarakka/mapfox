df402bc1d85557adcdc38bbc9d322292b1bed0f1
aleksanterif <aleksanteri.fagerholm@gmail.comgit config --global user.email aleksanteri.fagerholm@gmail.com>
2020-04-26 20:25:54 +0300

    user can now filter the helsinki api data with search words

--- MapFox/screens/AdventureCreationForm.js
+++ MapFox/screens/AdventureCreationForm.js
@@ -1,121 +1,111 @@
-import React from 'react';
-import {StyleSheet, Button, View, Text, TextInput } from 'react-native';
-import axios from 'axios'
-
-const SearchForm = (props) => {
-    return (
-      <View>
-        <TextInput placeHolder={'Etsi kohteita Helsingistä'} value={props.searchValue} onChange={props.handleSearch} />
-      </View>
-    ) 
-}
-  
-const Places = (props) => {
-
-        return (
-        
-          <View>
-            {props.results.map(result => <Text key={result.id} > {result.name.fi}</Text>)}
-          </View>
-        )
-    
-}
+import React, { useState, useEffect } from "react";
+import {
+    StyleSheet,
+    Button,
+    View,
+    Text,
+    TextInput,
+    ScrollView,
+} from "react-native";
+import axios from "axios";
 
 const PlacesView = () => {
-    const [places, setPlaces] = React.useState([])
-    const [searchValue, setSearchValue] = React.useState('')
-  
-    const handleSearch = (event) => {
-      console.log(event.target.value)
-      setSearchValue(event.target.value)
-    }
-  
-    // Vanilla React JS:l toimii tää filtteröinti funktio mikä lisää resultseihi placesit jotka mätsää hakutekstin kanssa
-    // TODO: Why won't it work?! Pls fix ( ͡° ͜ʖ ͡°)
-    const results = places.filter(place => place.name.fi.indexOf(searchValue) != -1)
+    const [places, setPlaces] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+
+    useEffect(() => {
+        axios.get("http://open-api.myhelsinki.fi/v1/places/").then((response) => {
+            console.log("promise fulfilled");
+            setPlaces(response.data.data);
+        });
+    }, []);
 
 
 
-    React.useEffect(() => {
-      console.log('effect')
-      axios
-        .get('http://open-api.myhelsinki.fi/v1/places/')
-        .then(response => {
-          console.log('promise fulfilled')
-          setPlaces(response.data.data)
-        })
-    }, [])
+    useEffect(() => {
+        const results = places.filter(
+            place => place.name.fi.toLowerCase().includes(searchValue.toLowerCase())
+        );
+        setSearchResults(results);
+    }, [searchValue]);
 
-    console.log('render', places.length, 'places')
-    console.log(places)
-  
+
     return (
-
-      <View>
-      <SearchForm searchValue={searchValue} handleSearch={handleSearch} />
-      <Places results={results}/>
-      
-  
-      </View>
-    )
- }
+        <View style={styles.places}>
+            <View>
+                <TextInput
+                    placeHolder={"Etsi kohteita Helsingistä"}
+                    value={searchValue}
+                    onChangeText={searchValue => setSearchValue(searchValue)}
+                />
+            </View>
+            <ScrollView>
+                <View>
+                    {searchResults.map((item) => (
+                        <Text key={item.id}> {item.name.fi}</Text>
+                    ))}
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
 
 const AdventureCreationForm = () => {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [places, setPlaces] = useState("");
 
-    const [title, setTitle] = React.useState('')
-    const [description, setDescription] = React.useState('')
-    const [places, setPlaces] = React.useState('')
-
-
-    const [eventObject, setEventObject] = React.useState({
-        title: '',
-        description: '',
-        places: [{
-            name: '',
-            latitude: 0.0,
-            longitude: 0.0
-        }]
-    })
+    const [eventObject, setEventObject] = useState({
+        title: "",
+        description: "",
+        places: [
+            {
+                name: "",
+                latitude: 0.0,
+                longitude: 0.0,
+            },
+        ],
+    });
 
     const handleChange = (event) => {
-        setEventObject({ ...eventObject, [event.target.name]: event.target.value })
-    }
+        setEventObject({ ...eventObject, [event.target.name]: event.target.value });
+    };
 
     const addEventObject = (event) => {
-        event.preventDefault()
+        event.preventDefault();
         const eventObject = {
             title: newTitle,
             description: newDescription,
-            places: [{
-                newName: '',
-                newLatitude: 0.0,
-                newLongitude: 0.0
-            }]
-        }
-    
-    }
+            places: [
+                {
+                    newName: "",
+                    newLatitude: 0.0,
+                    newLongitude: 0.0,
+                },
+            ],
+        };
+    };
 
     const postEventObject = (event) => {
-        event.preventDefault
-    
+        event.preventDefault;
+
         const eventObject = {
             title: title,
             description: description,
-            places: [{
-                name: 'b',
-                latitude: 0.0,
-                longitude: 0.0
-            }]
-        }
+            places: [
+                {
+                    name: "b",
+                    latitude: 0.0,
+                    longitude: 0.0,
+                },
+            ],
+        };
 
-        axios
-        .post('http://128.199.36.67/events', eventObject)
-        .then(response => {
-            console.log(response)
-        })
-    }
-
-   
+        axios.post("http://128.199.36.67/events", eventObject).then((response) => {
+        });
+    };
 
     return (
         // TODO Scroll Bar
@@ -125,37 +115,40 @@ const AdventureCreationForm = () => {
         <View style={styles.container}>
             <Text>Create an adventure here</Text>
             <TextInput
-                placeholder={'Otsikko'}
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                onChangeText={title => setTitle(title)}
+                placeholder={"Otsikko"}
+                style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+                onChangeText={(title) => setTitle(title)}
                 value={title}
-                />
+            />
             <TextInput
-                placeholder={'Kuvaus tapahtumasta'}
-                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                onChangeText={description => setDescription(description)}
+                placeholder={"Kuvaus tapahtumasta"}
+                style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+                onChangeText={(description) => setDescription(description)}
                 value={description}
             />
             <PlacesView />
 
-            
-
-            <Button title={'Press'} onPress={postEventObject}></Button>
-
+            <Button title={"Press"} onPress={postEventObject}></Button>
         </View>
     );
 };
 
-
-AdventureCreationForm.navigationOptions= {
-    headerTitle: 'My adventures'
-  };
+AdventureCreationForm.navigationOptions = {
+    headerTitle: "My adventures",
+};
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff'
-    }
+        backgroundColor: "#fff",
+    },
+    places: {
+        marginTop: 100,
+        padding: 20,
+        backgroundColor: "#ccc",
+        borderColor: "black",
+        borderWidth: 1,
+    },
 });
 
 export default AdventureCreationForm;
