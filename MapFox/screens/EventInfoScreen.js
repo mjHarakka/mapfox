cfb991876873fb8d9e61f7c78236ccf32d87943c
aleksanterif <aleksanteri.fagerholm@gmail.comgit config --global user.email aleksanteri.fagerholm@gmail.com>
2020-05-10 23:33:41 +0300

    added final touch to event info screen

--- MapFox/screens/EventInfoScreen.js
+++ MapFox/screens/EventInfoScreen.js
@@ -1,41 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, Button } from 'react-native';
-import MapView, { Marker, Heatmap } from 'react-native-maps';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
-import Colors from '../constants/Colors';
-import EmbeddedMap from '../components/EmbeddedMap';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
+import MapView, { Marker, Heatmap } from "react-native-maps";
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
+import Colors from "../constants/Colors";
+import EmbeddedMap from "../components/EmbeddedMap";
+import { ScrollView } from "react-native-gesture-handler";
 
 const EventInfoScreen = (props) => {
-    const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("")
-    const [places, setPlaces] = useState([{}])
-    const [open, setOpen] = useState(false)
-    const [location, setLocation] = useState(null);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [places, setPlaces] = useState([{}]);
+    const [open, setOpen] = useState(false);
     const { params } = props.navigation.state;
 
-
     useEffect(() => {
-        setTitle(params.title)
-        setDescription(params.description)
-        setPlaces(params.places)
-        getLocation()
-    }), [];
-
-    const getLocation = async () => {
-        let { status } = await Permissions.askAsync(Permissions.LOCATION);
-        if (status !== 'granted') {
-            Alert.alert('No permission to access location');
-        } else {
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-        }
-    };
+        setTitle(params.title);
+        setDescription(params.description);
+        setPlaces(params.places);
+    }),
+        [];
 
     const showmap = () => {
-        setOpen(true)
-    }
+        setOpen(true);
+    };
+
+    const markers = [
+        {
+            latlng: { latitude: places.latitude, longitude: places.longitude },
+            title: places.title,
+            description: places.description,
+        },
+    ];
 
     return (
         <View style={styles.container}>
@@ -51,69 +47,108 @@ const EventInfoScreen = (props) => {
                             <Text style={styles.text}>{description}</Text>
                         </View>
                     </View>
-                    <Button color={Colors.secondaryOrange} title="Show map" onClick={() => showmap}></Button>
+                    <Text style={styles.mapTitle}>Event markers</Text>
                     <View>
                         <View style={styles.mapContainer}>
-                            {/* Tähän tulisi karttanäkymä esille, joka open staten muuttuessa
-                    trueksi piirrettäisiin näkyville */}
+                            <MapView
+                                style={{ flex: 1 }}
+                                initialRegion={{
+                                    latitude: places == undefined ? 60.201373 : places[0].latitude,
+                                    longitude: places == undefined ? 24.934041 : places[0].longitude,
+                                    latitudeDelta: 0.0322,
+                                    longitudeDelta: 0.0221,
+                                }}
+                            >
+                                {places.map(
+                                    (marker, index) => (
+                                        console.log(places),
+                                        (
+                                            <Marker
+                                                key={index}
+                                                coordinate={{
+                                                    latitude:
+                                                        places == null ? 0 : marker.latitude,
+                                                    longitude:
+                                                        places == null ? 0 : marker.longitude,
+                                                }}
+                                                title={places == null ? "" : marker.name}
+                                                description={places == null ? "" : marker.description}
+                                                pinColor="blue"
+                                            />
+                                        )
+                                    )
+                                )}
+                            </MapView>
                         </View>
                     </View>
                 </View>
             </ScrollView>
         </View>
-    )
-
-}
+    );
+};
 EventInfoScreen.navigationOptions = {
-    headerTitle: 'Event info'
+    headerTitle: "Event info",
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
         borderColor: Colors.secondaryBlue,
         borderWidth: 15,
-
     },
     subContainer: {
-        margin: 10
+        margin: 10,
     },
     title: {
-        textAlign: 'center',
+        textAlign: "center",
         fontSize: 22,
-        fontWeight: 'bold',
-        borderBottomColor: 'black',
+        fontWeight: "bold",
+        borderBottomColor: "black",
         borderBottomWidth: 2,
         backgroundColor: Colors.secondaryOrange,
         marginBottom: 5,
         padding: 5,
         borderTopLeftRadius: 7,
         borderTopRightRadius: 7,
-        color: Colors.primaryBlue
+        color: Colors.primaryBlue,
+    },
+    mapTitle: {
+        textAlign: "center",
+        fontSize: 22,
+        fontWeight: "bold",
+        borderBottomColor: "black",
+        borderBottomWidth: 2,
+        backgroundColor: Colors.secondaryOrange,
+        marginBottom: 5,
+        padding: 5,
+        borderTopLeftRadius: 7,
+        borderTopRightRadius: 7,
+        color: Colors.primaryBlue,
+        borderColor: Colors.primaryBlue,
+        borderWidth: 2
     },
     text: {
-        textAlign: 'center',
+        textAlign: "center",
         fontSize: 22,
-        fontWeight: 'bold',
+        fontWeight: "bold",
         borderColor: Colors.secondaryBlue,
         margin: 5,
-        padding: 5
+        padding: 5,
     },
     wrapper: {
         borderWidth: 2,
         borderColor: Colors.secondaryBlue,
         borderRadius: 10,
-        margin: 10
+        margin: 10,
     },
     mapContainer: {
         flex: 1,
         height: 250,
     },
     map: {
-        height: 250
-    }
-
+        height: 250,
+    },
 });
 
 export default EventInfoScreen;
